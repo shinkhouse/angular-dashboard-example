9dcee37bb5e02877774cf5326574a9724b660694
Samuel Hinkhouse <hinkhouse.samuel@gmail.com>
2023-11-13 19:32:49 -0600

    wip: create some dummy autocomplete for the search bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,6 +2,9 @@ import { Component, Renderer2, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatOptionModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
 export type Density = 'comfortable' | 'cozy' | 'compact';
 export interface DensityOption {
     value: Density;
@@ -11,11 +14,28 @@ export interface DensityOption {
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [CommonModule, MatIconModule, MatMenuModule],
+    imports: [CommonModule, MatIconModule, MatMenuModule, MatAutocompleteModule, MatOptionModule, MatButtonModule],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+    public gemstoneProducts = [
+        { name: 'Ruby', partNumber: 'RUB-' + Math.floor(Math.random() * 10000) },
+        { name: 'Emerald', partNumber: 'EME-' + Math.floor(Math.random() * 10000) },
+        { name: 'Sapphire', partNumber: 'SAP-' + Math.floor(Math.random() * 10000) },
+        { name: 'Diamond', partNumber: 'DIA-' + Math.floor(Math.random() * 10000) },
+    ];
+
+    public salesOrders = Array.from({ length: 3 }, () => 'SO-' + Math.floor(Math.random() * 10000));
+    public purchaseOrders = Array.from({ length: 3 }, () => 'PO-' + Math.floor(Math.random() * 10000));
+    public invoiceNumbers = Array.from({ length: 3 }, () => 'IN-' + Math.floor(Math.random() * 10000));
+
+    public searchResults = [
+        { category: 'Inventory', results: this.gemstoneProducts.map((p) => p.partNumber) },
+        { category: 'Sales', results: this.salesOrders },
+        { category: 'Purchasing', results: this.purchaseOrders },
+        { category: 'Invoices', results: this.invoiceNumbers },
+    ];
     private dark: boolean = false;
     public densityOptions: DensityOption[] = [
         { name: 'Comfortable', value: 'comfortable', icon: 'density_large' },
